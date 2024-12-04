903917b63bec333d86006c3700ebb4aec2190a14
AllanRono1 <allanrono18@gmail.com>
2024-12-04 13:05:19 +0300

    FIXED 404 error

--- index.php
+++ index.php
@@ -2,12 +2,14 @@
 
 require "functions.php";
 
+//dd($_SERVER);
+
 $uri = $_SERVER['REQUEST_URI'];
 
 if ($uri === '/') {
     require 'controllers/index.php';
 } else if ($uri === '/about') {
     require 'controllers/about.php';
-} else if ($uri === 'contact') {
+} else if ($uri === '/contact') {
     require 'controllers/contact.php';
 }
