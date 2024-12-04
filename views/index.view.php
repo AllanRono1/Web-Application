10f2dcd2204ee748f193fe0de47516d19f999f2f
AllanRono1 <allanrono18@gmail.com>
2024-12-04 11:05:52 +0300

    ADD Routers and page links

---
+++ views/index.view.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" class ="h-full bg-gray-100">
+<?php require "partials/header.php" ?>
+
+<body class ="h-full">
+<div class="min-h-full">
+  <?php require "partials/nav.php" ?>
+<?php require "partials/banner.php" ?>
+  <main>
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+      <p>Hello There</p>
+    </div>
+  </main>
+<?php require "partials/footer.php" ?>
