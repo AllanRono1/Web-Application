db59e7e4a000925cd9989a5e50e66110ed80feca
AllanRono1 <allanrono18@gmail.com>
2024-12-05 11:08:48 +0300

    ADD status code and did some refactoring

---
+++ views/404.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" class ="h-full bg-gray-100">
+<?php require "partials/header.php" ?>
+
+<body class ="h-full">
+<div class="min-h-full">
+  <?php require "partials/nav.php" ?>
+
+  <main>
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+      <h2 class = "text-2xl font-bold">404 error</h2>
+      <a href ='/' class ="text-blue underline">home</a>
+    </div>
+  </main>
+<?php require "partials/footer.php" ?>
