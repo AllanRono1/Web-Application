81717faf95f98b8f423f591a5b1e9f04be0f38dc
AllanRono1 <allanrono18@gmail.com>
2024-12-06 16:44:13 +0300

    CONNECT MySQL from php and execute a simple SELECT query

--- index.php
+++ index.php
@@ -3,3 +3,15 @@
 require "functions.php";
 
 require "router.php";
+
+$dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
+
+$pdo = new PDO($dsn );
+
+$statement = $pdo->prepare("select * from posts");
+
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+dd($posts);
--- router.php
+++ router.php
@@ -1,17 +1,8 @@
 <?php
 
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-
 //dd($uri);
 
-
-if ($uri === '/') {
-    require 'controllers/index.php';
-} else if ($uri === '/about') {
-    require 'controllers/about.php';
-} else if ($uri === '/contact') {
-    require 'controllers/contact.php';
-}
 
  $routes = [
     '/' => 'controllers/index.php',
