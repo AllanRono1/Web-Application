2c57e7c348e574c2a1d10fbc2dcf737e406a721a
AllanRono1 <allanrono18@gmail.com>
2025-05-03 22:32:40 +0300

    Did some server-side form validation

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -3,15 +3,21 @@
 $config = require "config.php";
 $db = new Database($config['database']);
 
-//dd($db);
-
 $heading = "Create Your Notes";
 
 if ($_SERVER ['REQUEST_METHOD'] === 'POST'){
-    $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id',[
+$error = [];
+    if(strlen($_POST['body']) === 0){
+        $error['body'] = 'can not be empty';
+    }
+
+    if (empty($error)) {
+    $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)',[
         'body' => $_POST['body'],
         'user_id' => 1
+    
     ]);
+}
 }
 
 require "views/create-notes.view.php";
--- views/create-notes.view.php
+++ views/create-notes.view.php
@@ -13,6 +13,10 @@
           <label for="body" class="block text-sm/6 font-medium text-gray-900"></label>
           <div class="mt-2">
             <textarea name="body" id="body" rows="3" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"></textarea>
+
+            <?php if (isset($error['body'])) : ?>
+            <?= $error ['body'] ?>
+            <?php endif ; ?>
           </div>
           <p class="mt-3 text-sm/6 text-gray-600">Write a few sentences about yourself.</p>
         </div>
